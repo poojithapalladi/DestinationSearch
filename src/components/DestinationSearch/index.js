fc8e13e2affdbaf5c1395d44cb7749b1778d7bb2
poojita <poojithapalladi@gmail.com>
2024-03-24 09:13:41 +0000

    test cases failed

---
+++ src/components/DestinationSearch/index.js
@@ -0,0 +1,10 @@
+import './index.css'
+
+import DestinationItem from './DestinationItem'
+
+import DestinationSearch from 'react'
+;<div className="img-container">
+  <h1 className="heading">Destination Search</h1>
+  <input type="search" />
+</div>
+export default DestinationSearch
